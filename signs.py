ac3ed33998dcecfb10b561121c58ead88d1f86ba
sanchezg <gonchi.sanchez@gmail.com>
2017-10-09 21:26:02 -0300

    Import fixes

--- signs.py
+++ signs.py
@@ -1,6 +1,6 @@
 import requests
 import pytz
-from datetime import datetime
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 from .utils import _setup_debug_logger
 
@@ -17,16 +17,16 @@ logger = _setup_debug_logger(__name__)
 def get_day_based_on_tz(day, tz):
     """Gets the client date() based on tz passed as parameter.
     """
-    server_day = datetime.now(tz=pytz.timezone(SERVER_TIMEZONE)).date()
+    server_day = datetime.now(tz=pytz.timezone(SERVER_TIMEZONE))
     day = 'today'
     if tz is not None and tz in pytz.all_timezones:
         client_day = server_day.astimezone(pytz.timezone(tz)).date()
         # else not necessary, same day
         asked = TIMEDELTA_DAYS[day]
-        asked_date = client_day + datetime.timedelta(days=asked)
-        if asked_date > server_day:
+        asked_date = client_day + timedelta(days=asked)
+        if asked_date > server_day.date():
             day = 'tomorrow'
-        elif asked_date < server_day:
+        elif asked_date < server_day.date():
             day = 'yesterday'
     return day
 
