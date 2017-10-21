2d32d5a8b66a4cdf58821e6586de934646daa185
gigkokman <kann.kupp@gmail.com>
2017-10-21 09:53:16 +0700

    Add signs tests

---
+++ tests/test_signs.py
@@ -0,0 +1,21 @@
+import unittest
+
+from aztro.signs import get_day_based_on_tz
+from aztro.signs import SERVER_TIMEZONE
+
+
+class TestSigns(unittest.TestCase):
+    def setUp(self):
+        self.tz = SERVER_TIMEZONE
+
+    def test_get_day_based_on_tz_yesterday(self,):
+        day = 'yesterday'
+        self.assertEqual(get_day_based_on_tz(day, self.tz), day)
+
+    def test_get_day_based_on_tz_today(self,):
+        day = 'today'
+        self.assertEqual(get_day_based_on_tz(day, self.tz), day)
+
+    def test_get_day_based_on_tz_tomorrow(self,):
+        day = 'tomorrow'
+        self.assertEqual(get_day_based_on_tz(day, self.tz), day)
