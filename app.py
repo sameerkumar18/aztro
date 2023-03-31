322c274819e8616bd5f7817bd1cac2a4a374109a
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-03-31 16:23:50 +0100

    change the request in app.py to perfom http call and use BeautifulSoup library to parse HTML

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import requests
 from flask import Flask, request, redirect, make_response
 from flask_cors import CORS
 from flask_restful import Resource, Api
+from bs4 import BeautifulSoup
+import requests
+
 
 from astrology import horoscope_info
 from utils import _setup_debug_logger
@@ -12,43 +15,40 @@ CORS(app, support_credentials=True)
 
 logger = _setup_debug_logger(__name__)
 
-signs = [
-    'aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra',
-    'scorpio', 'sagittarius', 'capricorn', 'aquarius', 'pisces'
-]
+signs = {
+    "aries": 1,
+    "taurus": 2,
+    "gemini": 3,
+    "cancer": 4,
+    "leo": 5,
+    "virgo": 6,
+    "libra": 7,
+    "scorpio": 8,
+    "sagittarius": 9,
+    "capricorn": 10,
+    "aquarius": 11,
+    "pisces": 12,
+}
 
 days = [
     'today', 'tomorrow', 'yesterday'
 ]
 
 
-class API(Resource):
-    def get(self):
-        # Source: https://github.com/sameerkumar18/aztro-landing-page
-        return make_response(
-            requests.get('https://cdn.jsdelivr.net/gh/sameerkumar18/aztro-landing-page/index.html').text)
+given_sign = "aries"
 
-    def post(self):
-        sign = request.args['sign'].lower()
-        day = request.args.get('day', 'today').lower()
-        timezone = request.args.get('tz')
-        try:
-            if (sign not in signs) or (day not in days):
-                raise Exception('Wrong sign or day passed. Please refer https://aztro.readthedocs.io/en/latest/ ')
-
-            response = horoscope_info(sign=sign, day=day, tz=timezone)
-            return response, 200
-        except Exception as e:
-            logger.error("{}".format(e))
-            return {'message': str(e)}, 400
+URL = "https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign=" + \
+    str(signs[given_sign])
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return redirect("/", code=302)
+response = requests.get(URL)
+soup = BeautifulSoup(response.text, 'html.parser')
 
+container = soup.find("p")
 
-api.add_resource(API, '/')
+print(container.text.strip())
+
+api.add_resource(response, '/')
 
 if __name__ == '__main__':
     app.run(debug=False, threaded=True)
